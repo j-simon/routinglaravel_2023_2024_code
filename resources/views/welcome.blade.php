e42f8d2cfd0bfc4c990b9c36774a53c26dac6720
Jens Simon <64278468+jens-simon@users.noreply.github.com>
2023-12-21 14:31:45 +0100

    Link zur Datenausgabe per View auf Startseite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -875,7 +875,10 @@
             <a href="/uebung_07_links.html" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[0.8] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                 Links zum Testen der Übung_07
             </a>
-
+            <br>
+            <a href="/daten_ausgeben" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[0.8] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                Daten per View ausgeben
+            </a>
 
 
             <div class="mt-16">
