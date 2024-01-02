14029e5bcde910a3991be4b77f27ee023c34481c
Jens Simon <64278468+jens-simon@users.noreply.github.com>
2024-01-02 12:59:07 +0100

    erklärung für 2.te Database Connection

--- config/database.php
+++ config/database.php
@@ -63,6 +63,7 @@ return [
             ]) : [],
         ],
 
+        // die 2. Database Connection, die Daten kommen aus der .env - Datei
         'mysql2' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
@@ -145,7 +146,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
